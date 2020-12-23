2eb5438a01c37abcd31df3e50e78af3e3dcf9d7a
John Dust <d24dust@hotmail.com>
2020-12-23 22:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function scuberGreetingForFeet(feet){
   // Write your code here!
+  switch true {
+    case feet < 400:
+      return "This one is on me!"
+    case feet > 2000:
+      return 
+  }
   
 }
 
